1349ef98b7d45f55e65551943125fa9c3f1b9a7c
Michael Medin <michael@medin.name>
2015-02-15 11:53:50 +0100

    Added support for running in "stand alone"

--- protoc-gen-json.cmd
+++ protoc-gen-json.cmd
@@ -2,5 +2,12 @@
 #!python.exe
 # EASY-INSTALL-SCRIPT: 'json-protobuf==0.0.1','protoc-gen-json'
 __requires__ = 'json-protobuf==0.0.1'
-import pkg_resources
-pkg_resources.run_script('json-protobuf==0.0.1', 'protoc-gen-json')
+try:
+	import pkg_resources
+	pkg_resources.run_script('json-protobuf==0.0.1', 'protoc-gen-json')
+except:
+	import sys
+	from os.path import dirname, realpath
+	path = dirname(realpath(__file__))
+	sys.path.append(path)
+	execfile("%s/protoc-gen-json"%path)
